1a0703724d3819ca9daa9c2295100d0f9c913411
Belle Beth Cooper <bellebethcooper@gmail.com>
2018-08-25 13:24:08 +1000

    Added file uploading

--- app/src/main/java/co/hellocode/micro/MainActivity.kt
+++ app/src/main/java/co/hellocode/micro/MainActivity.kt
@@ -4,12 +4,17 @@ import android.app.Activity
 import android.app.PendingIntent.getActivity
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.provider.CalendarContract
+import android.provider.MediaStore
 import android.support.design.widget.Snackbar
+import android.support.design.widget.TextInputEditText
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
@@ -19,15 +24,17 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.android.volley.Request
-import com.android.volley.Response
+import com.android.volley.*
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
-import com.android.volley.AuthFailureError
 import org.json.JSONObject
+import uk.me.hardill.volley.multipart.MultipartRequest
+import java.io.ByteArrayOutputStream
+import javax.security.auth.login.LoginException
 
+    const private val PICK_IMAGE = 1
 
 class MainActivity : AppCompatActivity() {
 
@@ -61,13 +68,21 @@ class MainActivity : AppCompatActivity() {
                     .show()
         }
 
-        val editText = findViewById<EditText>(R.id.editText)
         editText.requestFocus()
+
+        photoButton.setOnClickListener {
+            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getIntent.type = "image/*"
+            val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickIntent.type = "image/*"
+            val chooserIntent = Intent.createChooser(getIntent, "Select image")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, Array(1) { pickIntent })
+            startActivityForResult(chooserIntent, PICK_IMAGE)
+        }
 
         sendButton.setTextColor(resources.getColor(R.color.colorBackground))
         sendButton.setBackgroundColor(resources.getColor(R.color.colorPrimary))
         sendButton.setOnClickListener { view ->
-            val editText = findViewById<EditText>(R.id.editText)
             val text = editText.text.toString()
             val queue = Volley.newRequestQueue(this)
             val url = "https://micro.blog/micropub"
@@ -103,9 +118,88 @@ class MainActivity : AppCompatActivity() {
                     return params
                 }
             }
-            Log.i("MainActivity", rq.toString())
             queue.add(rq)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            Log.i("MainActivity", "User picked an image")
+
+            val stream = contentResolver.openInputStream(data.data)
+            val bitmap = BitmapFactory.decodeStream(stream)
+            stream.close()
+            val baos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val image = baos.toByteArray()
+            postImage(image)
+        }
+    }
+
+    private fun postImage(image: ByteArray) {
+        getMediaEndpoint(image)
+    }
+
+    fun uploadImage(endpoint: String, image: ByteArray) {
+        val headers = HashMap<String, String>()
+        val prefs = this@MainActivity.getSharedPreferences(PREFS_FILENAME, Context.MODE_PRIVATE)
+        val token: String? = prefs?.getString(TOKEN, null)
+        headers["Authorization"] = "Bearer $token"
+//        headers["Content-Type"] = "multipart/form-data"
+//        headers["Content-Length"] = "${image.size}"
+
+        val rq = MultipartRequest(endpoint,
+               headers,
+                Response.Listener {
+                    if (it != null) {
+                        Log.i("MainActivity", "Success! Resp: ${String(it.data)}")
+                    }
+                }
+                Response.ErrorListener {
+                    if (it.networkResponse != null) {
+                        Log.i("MainActivity", "Error: ${String(it.networkResponse.data)}")
+                    }
+                })
+        rq.addPart(MultipartRequest.FilePart("file", "image/jpeg", "file", image))
+        val queue = Volley.newRequestQueue(this)
+        queue.add(rq)
+    }
+
+    private fun getMediaEndpoint(image: ByteArray) {
+        val url = "https://micro.blog/micropub?q=config"
+        val rq = object : StringRequest(
+                Request.Method.GET,
+                url,
+                Response.Listener<String> { response ->
+                    Log.i("MainActivity", "resp: $response")
+                    val json = JSONObject(response)
+                    val endpoint = json["media-endpoint"] as String
+                    uploadImage(endpoint, image)
+                },
+                Response.ErrorListener { error ->
+                    Log.i("MainActivity", "err: $error msg: ${error.message}")
+                    // TODO: Handle error
+                })
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                val prefs = this@MainActivity.getSharedPreferences(PREFS_FILENAME, Context.MODE_PRIVATE)
+                val token: String? = prefs?.getString(TOKEN, null)
+                headers["Authorization"] = "Bearer $token"
+                return headers
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params["h"] = "entry"
+                return params
+            }
+        }
+        val queue = Volley.newRequestQueue(this)
+        queue.add(rq)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
