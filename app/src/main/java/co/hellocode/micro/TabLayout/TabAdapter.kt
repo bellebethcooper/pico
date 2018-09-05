f3c09ec77dbc30c07a64a69ac0f12777da15d8b7
Belle Beth Cooper <bellebethcooper@gmail.com>
2018-09-06 09:33:05 +1000

    Added photos tab

--- app/src/main/java/co/hellocode/micro/TabLayout/TabAdapter.kt
+++ app/src/main/java/co/hellocode/micro/TabLayout/TabAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import co.hellocode.micro.TabLayout.Fragments.DiscoverFragment
+import co.hellocode.micro.TabLayout.Fragments.MediaFragment
 import co.hellocode.micro.TabLayout.Fragments.MentionsFragment
 import co.hellocode.micro.TabLayout.Fragments.TimelineFragment
 
@@ -13,6 +14,7 @@ class TabAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         0 -> TimelineFragment.newInstance()
         1 -> MentionsFragment.newInstance()
         2 -> DiscoverFragment.newInstance()
+        3 -> MediaFragment.newInstance()
         else -> null
     }
 
@@ -20,8 +22,9 @@ class TabAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         0 -> "Timeline"
         1 -> "Mentions"
         2 -> "Discover"
+        3 -> "Photos"
         else -> ""
     }
 
-    override fun getCount(): Int = 3
+    override fun getCount(): Int = 4
 }
